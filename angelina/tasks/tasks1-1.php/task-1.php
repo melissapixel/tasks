23c8e8eaca45475f0589fc2d8207b93e975c9422
Mel Pixel <melissapixell@gmail.com>
2024-05-14 11:59:17 +0300

    task1

--- angelina/tasks/tasks1-1.php/task-1.php
+++ angelina/tasks/tasks1-1.php/task-1.php
@@ -7,6 +7,35 @@
 </head>
 <body>
     <?php
+        $num = 123;
+        echo $num;
+    ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
         error_reporting(E_ALL);
         ini_set('display_errors', 'on');
         mb_internal_encoding('UTF-8');
@@ -31,4 +60,4 @@
 </body>
 </html>
 
-
+ -->
