57649cb96afa40caf43fa04f65e269c4a2519495
pullock <pullock@yandex.ru>
2024-05-19 14:25:03 +0300

    tasks

---
+++ petr/theory/theory_110-147.php
@@ -0,0 +1,12 @@
+<?php
+    
+    foreach ($arr as $elem) {
+        echo $elem; // Элемент массива
+    }
+
+    foreach ($arr as $key => $elem) {
+        echo $key . '<br>'; // Ключ массива
+        echo $elem; // Элемент массива
+    }
+    
+?>
