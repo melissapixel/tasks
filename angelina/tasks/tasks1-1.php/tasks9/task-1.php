f10ddce2198d33b996f32847c9e3e0d4aeda917b
Mel Pixel <melissapixell@gmail.com>
2024-06-09 07:09:48 +0300

    morning09.06

--- angelina/tasks/tasks1-1.php/tasks9/task-1.php
+++ angelina/tasks/tasks1-1.php/tasks9/task-1.php
@@ -33,8 +33,23 @@
 <br>
 <?=  preg_replace('#\*(q)+\+#', 'M', '*+ *q+ *qq+ *qqq+ *qqq qqq+') ?>
 <br>
-<?=  preg_replace('[...]', 'M', '[abc] {abc} abc (abc)[abc]') ?>
-
+<?= preg_replace('[...]', 'M', '[abc] {abc} abc (abc)[abc]') ?>
+<br>
+<?= preg_replace('#ab{2,4}a#', 'k', 'aa aba abba abbba abbbba abbbbba'); ?>
+<br>
+<?= preg_replace('#ab{0,3}a#', 'k', 'aa aba abba abbba abbbba abbbbba'); ?>
+<br>
+<?= preg_replace('#ab{4,}a#', 'k', 'aa aba abba abbba abbbba abbbbba'); ?>
+<br>
+<?= preg_replace('#a\da#', '!', 'a1a a2a a3a a4a a5a aba aca') ?>
+<br>
+<?= preg_replace('#a\d*a#', '!', 'a1a a22a a333a a4444a a55555a aba aca') ?>
+<br>
+<?= preg_replace('#a\d*a#', '!', 'aa a1a a22a a333a a4444a a55555a aba aca') ?>
+<br>
+<?= preg_replace('#a\Db#', '!', 'avb a1b a2b a3b a4b a5b abb acb') ?>
+<br>
+<?= preg_replace('#a\wb#', '!', 'ave a#b a2b a$b a4b a5b a-b acb') ?>
 
 
 
