be28263a9a58cabb6d7357cad5ce4a7161224fc0
pullock <pullock@yandex.ru>
2024-06-12 20:57:17 +0300

    commit

---
+++ petr/theory/theory_185-228.php
@@ -0,0 +1,39 @@
+<?php
+
+    # Регулярные выражения
+
+    $str = 'ahb acb aeb aeeb adcb axeb';
+    preg_replace('#a.b#', 'ок', $str);
+
+    preg_replace('#a.+b#', 'ok', $str);
+
+    $str = 'ab abab abab abababab abea';
+
+    preg_replace('#(ab)+#', 'ok', $str);
+
+    $str = 'a.a aba aea';
+
+    preg_replace('#a\.a#', 'ok', $str);
+
+    $str = 'aa aba abba abbba abbbba abbbbba';
+
+    preg_replace('#ab{2,4}a#', 'ok', $str);
+
+    $str = 'aba accca azzza wwwwa';
+
+    preg_replace('#a.+a#', 'ok', $str);
+
+    $str = 'a1a a2a a3a a4a a5a aba aca';
+
+    preg_replace('#a\da#', 'ok', $str);
+
+    $str = 'aba aea aca aza axa';
+
+    preg_replace('#a[bex]a#', 'ok', $str);
+
+    $str = 'aAXa aeffa aGha aza ax23a a3sSa';
+
+    preg_replace('#a[a-z1-9]a#', 'ok', $str);
+
+    # Инвертирование наборов символов в регулярках
+?>
